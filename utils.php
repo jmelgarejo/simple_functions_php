70b3f6c541f0e3da8e18c1ed97e740b0bd813db3
jmelgarejo <juan.andres@puduhobbies.com>
2016-02-18 14:47:17 -0300

    Cambio en el nombre de la funcion log y agragdo ValidaRut como metodo estatico en la class Utils

--- utils.php
+++ utils.php
@@ -10,7 +10,7 @@ class Utils
 		# code...
 	}
 
-	static function log($valor){
+	static function simple_log($valor){
 		if(!file_exists('log')){
 			mkdir('log', 0777);
 		}
@@ -20,5 +20,20 @@ class Utils
 	    $fichero = "log/registro_{$fecha}.log";
 	    file_put_contents($fichero, "{$hora} {$valor}". PHP_EOL, FILE_APPEND);	
 	}
+
+	static function validaRut($rut){
+	    $temp_rut = str_replace(".", "",trim($rut));
+	    $RUT = explode("-", $temp_rut);
+	    $RUT = ($RUT[0]==$temp_rut)?array(substr($temp_rut, 0, -1), substr($temp_rut,-1) ):$RUT;
+	    $factor = 2;
+	    $suma=0;
+	    for($i = strlen($RUT[0])-1; $i >= 0; $i--){
+	        $factor = $factor > 7 ? 2 : $factor;
+	        $suma += $RUT[0][$i]*$factor++;
+	    }
+	    $dv = 11 - ($suma % 11);
+	    $dv = ($dv == 11)?0:($dv == 10)?"k":$dv;
+	    return ($dv == trim(strtolower($RUT[1])));
+	}	
 }
  ?>
