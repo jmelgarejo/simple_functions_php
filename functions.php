68840ffad1d1b654597b03afd9930bef8ef25cd4
jmelgarejo <juan.andres@puduhobbies.com>
2016-02-18 11:37:57 -0300

    Primer commit

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php 
+function tbk_log($valor){
+	if(!file_exists('log')){
+		mkdir('log', 0777);
+	}
+	$fecha = date('Ymd', time());
+	$hora = date('H:i:s', time());
+
+    $fichero = "log/registro_{$fecha}.log";
+    file_put_contents($fichero, "{$hora} {$valor}". PHP_EOL, FILE_APPEND);	
+}
+
+ ?>
